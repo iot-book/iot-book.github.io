470a00d1cf01fa419804cc2c99f91283f55a71a9
tongshuai <tongshuai.ts@gmail.com>
2019-11-06 14:53:42 +0800

    update

--- IoTBook/7.感知识别/智能手表手势识别/智能手表连接教程/ADB/Mac/connect.sh
+++
@@ -1,4 +0,0 @@
-#./connect
-./adb forward tcp:4444 localabstract:/adb-hub
-./adb connect 127.0.0.1:4444
-./adb devices
--- IoTBook/7.感知识别/智能手表手势识别/智能手表连接教程/ADB/Mac/pull.sh
+++
@@ -1,3 +0,0 @@
-#./pull ./Gesture/3D/A1
-fileName=$1
-./adb -s 127.0.0.1:4444  pull /sdcard/111.data $fileName.data
